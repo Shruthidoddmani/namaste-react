e62fc9ef55cd11f023d1ba9a5081c6abf10b6a74
Shruthi D A <shruthi.d@avoautomation.com>
2024-07-19 19:19:44 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const header =  React.createElement("h1", {id:'header'}, "hello World!!!")
+
+console.log(header);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(header);
